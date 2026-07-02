f7b0e3280a881f2fea3cedef1d35a5b898308d94
algnot <thanawat.tongla@gmail.com>
2026-07-02 15:31:28 +0700

    fix up

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -14,12 +14,12 @@ export default function RootLayout({
     <AuthGuard>
       <RoleGuard>
         <ReceiptPosModeShell>
-          <div className="md:flex min-h-screen">
-            <div data-app-chrome className="md:self-start">
+          <div className="md:flex min-h-screen md:h-screen md:overflow-hidden">
+            <div data-app-chrome className="shrink-0 md:h-full">
               <Menu />
             </div>
-            <div className="flex-1 flex flex-col min-w-0">
-              <div data-app-chrome>
+            <div className="flex min-h-0 flex-1 flex-col min-w-0 md:overflow-y-auto">
+              <div data-app-chrome className="shrink-0 md:sticky md:top-0 md:z-20">
                 <Top />
               </div>
               <main className="flex-1 pt-14 md:pt-0">
